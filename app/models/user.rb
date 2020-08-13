0aeb6cfdab2d5a1f330abb765f3d18ae31729a30
soruma <soruma.net@gmail.com>
2020-08-14 02:12:19 +0900

    Convert method from Twitter::User to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  def self.for_twitter_user_to_user(twitter_user)
+    user = find_or_initialize_by(id: twitter_user.id)
+
+    user.screen_name = twitter_user.screen_name
+    user.name = twitter_user.name
+    user.description = twitter_user.description.presence
+    user.profile_image_url = twitter_user.profile_image_url_https(:original)
+
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe '.for_twitter_user_to_user' do
+    let(:twitter_user) do
+      double('Twitter User',
+             id: 123,
+             screen_name: 'screen_name',
+             name: 'name',
+             description: 'description')
+    end
+    let(:user) { described_class.for_twitter_user_to_user(twitter_user) }
+
+    before do
+      allow(twitter_user).to receive(:profile_image_url_https).with(:original).and_return('url')
+    end
+
+    it 'convert argment to a user' do
+      expect(twitter_user).to receive(:profile_image_url_https).with(:original).and_return('url')
+
+      expect(user.class).to eq described_class
+      expect(user.id).to eq 123
+      expect(user.screen_name).to eq 'screen_name'
+      expect(user.name).to eq 'name'
+      expect(user.description).to eq 'description'
+    end
+
+    context 'user already exists' do
+      before { create(:user, id: 123, name: 'old_name') }
+
+      it 'user is update' do
+        expect(twitter_user).to receive(:profile_image_url_https).with(:original).and_return('url')
+
+        expect(user.id).to eq 123
+        expect(user.name).to eq 'name'
+      end
+    end
+
+    context 'witout description' do
+      let(:twitter_user) do
+        double('Twitter User',
+               id: 123,
+               screen_name: 'screen_name',
+               name: 'name',
+               description: Twitter::NullObject.new)
+      end
+
+      it 'convert argment to a user' do
+        expect(user.class).to eq described_class
+        expect(user.id).to eq 123
+        expect(user.description).to eq nil
+      end
+    end
+  end
 end
