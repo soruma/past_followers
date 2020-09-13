b2a9334b7f4e4d3abb150db1b96ff7e2d823eb65
soruma <soruma.net@gmail.com>
2020-09-13 22:04:05 +0900

    Create user list page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,47 @@
 <template>
   <div :class="$style.container">
-    <h1>Past followers</h1>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import gql from 'graphql-tag'
+import { User } from '~/lib/GraphQL/generated'
+
+interface Data {
+  users?: User[]
+}
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      users: undefined
+    }
+  },
+  computed: {
+    allUsers(): User[] {
+      if (this.users == null) return []
+      return this.users
+    }
+  },
+  apollo: {
+    users: gql`
+      query {
+        users {
+          id
+          name
+        }
+      }
+    `
+  }
+})
+</script>
 
 <style module>
 .container {
