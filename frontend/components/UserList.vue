bcee499ea4d5612532b05d3c65a2a9871249594f
soruma <soruma.net@gmail.com>
2020-09-22 19:08:54 +0900

    Create past followers page

--- frontend/components/UserList.vue
+++ frontend/components/UserList.vue
@@ -2,17 +2,19 @@
   <v-container>
     <v-row>
       <v-col v-for="user in users" :key="user.id" :cols="6">
-        <v-card>
-          <v-list-item>
-            <v-list-item-avatar size="70px" class="user-avatar">
-              <v-img :src="user.profileImageUrl"></v-img>
-            </v-list-item-avatar>
+        <n-link :to="`/past_followers/${user.id}`">
+          <v-card>
+            <v-list-item>
+              <v-list-item-avatar size="70px" class="user-avatar">
+                <v-img :src="user.profileImageUrl"></v-img>
+              </v-list-item-avatar>
 
-            <v-list-item-content>
-              <v-list-item-title v-text="user.name"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+              <v-list-item-content>
+                <v-list-item-title v-text="user.name"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </n-link>
       </v-col>
     </v-row>
   </v-container>
---
+++ frontend/pages/past_followers/_id.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>{{ this.user.name }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import gql from 'graphql-tag'
+import { User } from '~/lib/GraphQL/generated'
+
+interface Data {
+  user: User
+}
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      user: { name: '', createdAt: null, id: '', pastFollowers: [], updatedAt: null }
+    }
+  },
+  computed: {
+    findUser(): User {
+      if (this.user == null) return { name: '', createdAt: null, id: '', pastFollowers: [], updatedAt: null }
+      return this.user
+    }
+  },
+  apollo: {
+    user: {
+      query: gql`
+        query findUser($id: ID!) {
+          user(id: $id) {
+            id
+            name
+            profileImageUrl
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.$route.params.id
+        }
+      }
+    }
+  }
+})
+</script>
